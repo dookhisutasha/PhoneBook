b6e4a9a15df3d1a5bbd86210d43303dd037ee1f2
Dookhi <sutasha.dookhi@accenture.com>
2019-08-22 20:47:35 +0200

    Add project files.

---
+++ PhoneBookWebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace PhoneBookWebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.EnableCors();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
